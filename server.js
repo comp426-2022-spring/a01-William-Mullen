900a9b267060a24b410a436546db8fa467ae8a1b
William Mullen <69743259+William-Mullen@users.noreply.github.com>
2022-01-21 21:29:15 -0500

    add the listen message

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ const { sensitiveHeaders } = require('http2')
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
 const args = require('minimist')(process.argv.slice(2))
 // Define allowed argument name 'port'.
-args['port']
+args.port
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
-const port = args['port'] || 3000
+const port = args.port || 3000
 // Use the fs module to create an arrow function using `fs.readFile`.
 // Use the documentation for the Node.js `fs` module. 
 // The function must read a file located at `./www/index.html` and do some stuff with it.
@@ -31,14 +31,13 @@ fs.readFile('./www/index.html', 'utf8', (err, data) => {
 // 2. set a header with content type `text/html`, and 
 // 3. end with the data that you are reading in from ./www/index.html.
 const server = http.createServer((req, res) => {
-    response.statusCode = 200
-    response.setHeaders('name', text/html)
-    response.end(data)
+    res.statusCode = 200
+    res.setHeader('Content-Type', text/html)
+    res.end(data)
 })
 // Start the `server` const listening on the port defined by argument in your `port` const. 
 // Put the exact message `Server listening on port ${port}` on the console log. 
-
-
-
-
+server.listen(port, () => {
+    console.log(`Server listening at port ${port}`)
+})
 // That's it! You're all done!
