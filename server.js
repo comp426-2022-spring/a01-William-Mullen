ef52f5aacd4704a74b88fe24c365a52908fff02c
William Mullen <69743259+William-Mullen@users.noreply.github.com>
2022-01-27 17:13:39 -0500

    fixed minimist issue

--- server.js
+++ server.js
@@ -2,28 +2,26 @@
 const http = require('http')
 // Require fs module
 const fs = require("fs")
-//???????const { sensitiveHeaders } = require('http2')
 // Require minimist module (make sure you install this one via npm).
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
 const args = require('minimist')(process.argv.slice(2))
 // Define allowed argument name 'port'.
-args.port
+args["port"]
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
-const port = args.port || 3000
+const port = args.port || process.env.PORT || 3000
 // Use the fs module to create an arrow function using `fs.readFile`.
 // Use the documentation for the Node.js `fs` module. 
 // The function must read a file located at `./www/index.html` and do some stuff with it.
 // The stuff that should be inside this function is all below.
-fs.readFile('./www/index.html', 'utf8', (err, data) => {
+fs.readFile("./www/index.html", "utf8", (err, data) => {
 // If there is an error, put it on the console error, return, and exit with error code 1. 
 // Do not be nice about exiting.
     if (err) {
         console.error(err)
-        return 1
+        return
+        process.exit(1)
     }
-    console.log(data)
-})
 // Define a const `server` as an arrow function using http.createServer. 
 // Use the documentation for the node.js http module. 
 // The function should have three responses: 
@@ -41,3 +39,4 @@ server.listen(port, () => {
     console.log(`Server listening at port ${port}`)
 })
 // That's it! You're all done!
+})
